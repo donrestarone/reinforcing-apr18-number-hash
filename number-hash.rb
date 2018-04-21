e20dfe17b5238fc86f4963d4ed3998777d91283e
donrestarone <shashikejayatunge@gmail.com>
2018-04-21 11:28:13 -0400

    modula 2 function works

--- number-hash.rb
+++ number-hash.rb
@@ -1,17 +1,24 @@
 def number_hash
 	digit_hash = {}
+	key_counter = 0
+	value_counter = 0
+	
 	50.times do 
-		key_counter = 0
-		value_counter = 0
+		
 		key_counter += 1
 		value_counter += 1
 
-
 		digit_hash[key_counter] = value_counter
-		puts digit_hash
+		if value_counter % 2 == 0
+			digit_hash[key_counter] = (value_counter + 1)
+		#elsif value_counter %
+		end
+
+		
 		
 	end  
+	return digit_hash
 end 
 
-number_hash
+puts number_hash
 
