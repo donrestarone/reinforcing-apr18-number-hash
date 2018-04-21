2665118955178a4c8f7cc9af5e740f7b12dc700e
donrestarone <shashikejayatunge@gmail.com>
2018-04-21 11:20:33 -0400

    testing times loop, however it is not advancing the counter as expected

--- number-hash.rb
+++ number-hash.rb
@@ -1,7 +1,17 @@
 def number_hash
 	digit_hash = {}
 	50.times do 
-		counter += 1
-		digit_hash[number] = 
+		key_counter = 0
+		value_counter = 0
+		key_counter += 1
+		value_counter += 1
+
+
+		digit_hash[key_counter] = value_counter
+		puts digit_hash
+		
 	end  
 end 
+
+number_hash
+
