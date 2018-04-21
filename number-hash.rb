12d9089cfc8dba5c8082b41bd3b987f0c87407a5
donrestarone <shashikejayatunge@gmail.com>
2018-04-21 14:44:40 -0400

    finish exercise

--- number-hash.rb
+++ number-hash.rb
@@ -4,28 +4,40 @@ def number_hash
 	digit_hash = {}
 	key_counter = 0
 	value_counter = 0
+
+ 
+		
+		50.times do 
+			
+			key_counter += 1
+			value_counter += 1
+
+			digit_hash[key_counter] = value_counter
+			
+				if value_counter % 2 == 0 && value_counter % 7 == 0
+					digit_hash[key_counter] = (value_counter * 2)
+				#end
+
+				elsif value_counter % 2 == 0
+					digit_hash[key_counter] = (value_counter + 1)
+				#end
+
+				elsif value_counter % 7 == 0
+					digit_hash[key_counter] = (value_counter - 1)
+				end
+		end  
+		return digit_hash
+		
 	
-	50.times do 
-		
-		key_counter += 1
-		value_counter += 1
-
-		digit_hash[key_counter] = value_counter
-		
-			if value_counter % 2 == 0 && value_counter % 7 == 0
-				digit_hash[key_counter] = (value_counter * 2)
-			#end
-
-			elsif value_counter % 2 == 0
-				digit_hash[key_counter] = (value_counter + 1)
-			#end
-
-			elsif value_counter % 7 == 0
-				digit_hash[key_counter] = (value_counter - 1)
-			end
-	end  
-	return digit_hash
 end 
 
+
+
+# Benchmark.bmbm(1)do|bm|
+
 puts number_hash
+# bm.report('reverse')do
+
+# end
+# end
 
